3993b8a14f6038ae2923e935c839668753f2ec82
maxjacobzander <maxjacobzander@gmail.com>
2020-10-05 10:47:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
+configure do
+    enable :sessions
+    set :session_secret, "secretlabtestsetting"
+end
+
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
+
 end
